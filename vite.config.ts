dc893b282d5f89b2e73a0c9bcf35a6782d2e9b7f
CHOIYEJUN <dpwns108@gmail.com>
2025-04-16 00:54:42 +0900

    feat: npm 세팅
    - 빌드 및 npm 퍼블리싱 완료
    
    #feat/npmSetting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), vanillaExtractPlugin()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/package/component/Select.tsx'),
+      name: 'KokoReactSelect',
+      fileName: () => 'Select.js',
+      formats: ['es'],
+    },
+    outDir: 'src/package/dist',
+    emptyOutDir: false,
+    rollupOptions: {
+      external: (source) => {
+        return ['react', 'react-dom'].includes(source) || source.endsWith('.svg?react');
+      },
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
